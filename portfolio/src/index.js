c444122d3af690d559f3ba3ff135909e14d307de
Ankita Shahi <ankysingh6727@gmail.com>
2026-04-29 14:45:52 +0000

    portfolioGithubadderror

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
 
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
+import About from './components/About/About';
+const router=createBrowserRouter([
+  {
+path:"/",
+element:<Layout/>,
+children:[
+  {
+path:"",
+element:<Home/>
+  },
+  {
+path:"About",
+element:<About/>
+
+  },
+  {
+    path:"Contact",
+    element:<Contact/>
+  }
+]
+}
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
